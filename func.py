f5aa77e5821cee764c4a752758c0cf35e6965fc1
andygarciaserra <alu0101451923@ull.edu.es>
2022-11-23 12:59:47 +0000

    .

--- func.py
+++ func.py
@@ -55,8 +55,11 @@ def Img_calibration(DATADIR, plot):
     atlas = Import_data(DATADIR+'atlas_6301_6302.fits')
     data = Import_data(DATADIR+'Stokes_sunspot_HINODE.fits')
     hinode = np.mean(data[0,25:75,325:375,:], axis=(0,1))
+    hinodeT = np.vstack((range(96),hinode)).T  #--------------------> TOMAR ESTO COMO "HINODE" Y REPETIR EL PROCESO DE
+                                                                    # FITEAR LOS MÍNIMOS Y HACER LA CURVA DE CALIBRACIÓN
+                                                                    # CON LOS MÍNIMOS DE LOS FITS
 
-    #Creating atlas surrounding vectors for both minimum fits
+    #Creating atlas surrounding vectors for both minimum fits       
     dlambda = 15
     min1_pos = np.argmin(atlas[740:780,1]) + 740
     min2_pos = np.argmin(atlas[1225:1275,1]) + 1225
@@ -69,19 +72,25 @@ def Img_calibration(DATADIR, plot):
     min1_fit = np.polyfit(x_min1_surr,I_min1_surr,2)
     min2_fit = np.polyfit(x_min2_surr,I_min2_surr,2)
     min1fit_pos = Find_minimum(min1_fit,x_min1_surr)
-    print('Minimum 1 in fit: '+str(x_min1_surr[min1fit_pos]))
     min2fit_pos = Find_minimum(min2_fit,x_min2_surr)
-    print('Minimum 2 in fit: '+str(x_min2_surr[min2fit_pos]))
     min1atlas_pos = np.argmin(np.absolute(atlas[:,0]-x_min1_surr[min1fit_pos]))
     min2atlas_pos = np.argmin(np.absolute(atlas[:,0]-x_min2_surr[min2fit_pos]))
-    print('Minimum 1 in atlas: '+str(atlas[min1atlas_pos,0]))
-    print('Minimum 2 in atlas: '+str(atlas[min2atlas_pos,0]))
+    min1hinode_pos = np.argmin(hinode[20:30]) + 20
+    min2hinode_pos = np.argmin(hinode[60:80]) + 60
+    min1_factor = hinode[0]/atlas[0,1]
+    min2_factor = hinode[0]/atlas[0,1]
+    factor = np.mean([min1_factor,min2_factor])
 
     #Final calibration
-    curve = Calibration_Curve([atlas[]],[])
-                # Me falta encontrar los mínimos en hinode y con esos mínimos
-                # y los del fit hacer la curva de calibración
+    curve = Calibration_Curve([min1hinode_pos, min2hinode_pos],
+                              [atlas[min1atlas_pos,0], atlas[min2atlas_pos,0]])
+    lamda = curve[0]*range(96)+curve[1]
+    atlas[:,1] = factor*atlas[:,1]
 
+    plt.figure()
+    plt.plot(lamda,hinode)
+    plt.plot(atlas[:,0],atlas[:,1])
+    plt.show()
 
     #Plotting
     if (plot=='yes'):
@@ -109,5 +118,4 @@ def Find_minimum(fit, xcoord):
     return minpos
 
 def Calibration_Curve(p1,p2):
-
-    return np.polyfit((p1,p2),1)
+    return np.polyfit(p1,p2,1)
