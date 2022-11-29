1519100ec32cb36363640b06841fafe12336eef4
andygarciaserra <alu0101451923@ull.edu.es>
2022-11-29 13:27:55 +0000

    .

--- func.py
+++ func.py
@@ -9,16 +9,14 @@ from sympy import *
 
 
 
-##ESPECTRA CALIBRATION##
-
-    #Importing data from fits
+#Importing data from fits
 def Import_data(dir):
     data = fits.getdata(dir)
     return data
 
-    #Creating raw intensity image at t=50
-def print_img(figdir,DATADIR):
-    data = Import_data(DATADIR+'Stokes_sunspot_HINODE.fits')
+#Creating raw intensity image at t=50
+def print_img(figdir):
+    data = Import_data(figdir)
     hdu = fits.PrimaryHDU(data[0,:,:,50])
     hdu.writeto('new.fits')
     I_img = fits.getdata('new.fits',ext=0)
@@ -29,13 +27,6 @@ def print_img(figdir,DATADIR):
     plt.tight_layout()
     plt.savefig(figdir+'Full_Image.png', dpi=150)
     plt.show()
-        
-    #Plotting spectra for calm and dark regions
-def Import_regions(data):
-    I = data[0,:,:,:]           #Calm region at (350,50)
-    I_calm = I[25:75,325:375,:]
-    I_dark = I[175:200,225:250,:]
-    return(I_calm,I_dark)
 
 def Mean_Spectra_regions(FIGDIR,I_calm,I_dark):
     mean_calm = np.mean(I_calm, axis=(0,1))
@@ -47,65 +38,53 @@ def Mean_Spectra_regions(FIGDIR,I_calm,I_dark):
     plt.savefig(FIGDIR+'Calm_vs_Dark_t50.png', dpi=150)
     plt.show()
     return(mean_calm,mean_dark)
-    
+
+def Double_Minimum_Finder(spectra,delta,surr_min1,surr_min2):
+    xmin1,ymin1 = Minimum_Finder(spectra,delta,surr_min1)
+    xmin2,ymin2 = Minimum_Finder(spectra,delta,surr_min2)
+    return [xmin1,xmin2,ymin1,ymin2]
+
+def Minimum_Finder(spectra,delta,surr):
+    min_pos = np.argmin(spectra[surr[0]:surr[1],1]) + surr[0]
+    I_min_surr = spectra[min_pos-delta:min_pos+delta,1]
+    x_min_surr = spectra[min_pos-delta:min_pos+delta,0]
+    min_fit = np.polyfit(x_min_surr,I_min_surr,2)
+    min_cont = np.linspace(spectra[min_pos-delta,0],spectra[min_pos+delta,0],1000)
+    minfit_pos = Find_minimum(min_fit,min_cont)
+    minfit_y = min_fit[0]*min_cont[minfit_pos]**2 + min_fit[1]*min_cont[minfit_pos]+min_fit[2]
+    return [min_cont[minfit_pos],minfit_y]
+
+
 # Calibration of HINODE data using atlas high reslution spectra
 def Img_calibration(DATADIR, plot):
     
     #Importing data
     atlas = Import_data(DATADIR+'atlas_6301_6302.fits')
     data = Import_data(DATADIR+'Stokes_sunspot_HINODE.fits')
-    hinode = np.mean(data[0,25:75,325:375,:], axis=(0,1))
-    hinodeT = np.vstack((range(96),hinode)).T  #--------------------> TOMAR ESTO COMO "HINODE" Y REPETIR EL PROCESO DE
-                                                                    # FITEAR LOS MÍNIMOS Y HACER LA CURVA DE CALIBRACIÓN
-                                                                    # CON LOS MÍNIMOS DE LOS FITS
+    hinode = np.vstack((range(96),np.mean(data[0,25:75,325:375,:],axis=(0,1)))).T
 
     #Creating atlas surrounding vectors for both minimum fits       
-    dlambda = 15
-    min1_pos = np.argmin(atlas[740:780,1]) + 740
-    min2_pos = np.argmin(atlas[1225:1275,1]) + 1225
-    I_min1_surr = atlas[min1_pos-dlambda:min1_pos+dlambda,1]
-    x_min1_surr = atlas[min1_pos-dlambda:min1_pos+dlambda,0]
-    I_min2_surr = atlas[min2_pos-dlambda:min2_pos+dlambda,1]
-    x_min2_surr = atlas[min2_pos-dlambda:min2_pos+dlambda,0]
-
-    #Fitting to find minimums & printing
-    min1_fit = np.polyfit(x_min1_surr,I_min1_surr,2)
-    min2_fit = np.polyfit(x_min2_surr,I_min2_surr,2)
-    min1fit_pos = Find_minimum(min1_fit,x_min1_surr)
-    min2fit_pos = Find_minimum(min2_fit,x_min2_surr)
-    min1atlas_pos = np.argmin(np.absolute(atlas[:,0]-x_min1_surr[min1fit_pos]))
-    min2atlas_pos = np.argmin(np.absolute(atlas[:,0]-x_min2_surr[min2fit_pos]))
-    min1hinode_pos = np.argmin(hinode[20:30]) + 20
-    min2hinode_pos = np.argmin(hinode[60:80]) + 60
-    min1_factor = hinode[0]/atlas[0,1]
-    min2_factor = hinode[0]/atlas[0,1]
-    factor = np.mean([min1_factor,min2_factor])
+    xcoords_atlas = Double_Minimum_Finder(atlas,15,[740,780],[1225,1275])
+    xcoords_hinode = Double_Minimum_Finder(hinode,8,[15,35],[60,80])
+    min1_factor = hinode[0,1]/atlas[0,1]
+    min2_factor = hinode[0,1]/atlas[0,1]
+    vfactor = np.mean([min1_factor,min2_factor])
 
     #Final calibration
-    curve = Calibration_Curve([min1hinode_pos, min2hinode_pos],
-                              [atlas[min1atlas_pos,0], atlas[min2atlas_pos,0]])
-    lamda = curve[0]*range(96)+curve[1]
-    atlas[:,1] = factor*atlas[:,1]
+    curve = Calibration_Curve([xcoords_hinode[0], xcoords_hinode[1]],
+                              [xcoords_atlas[0], xcoords_atlas[1]])
+    atlas[:,1] = vfactor*atlas[:,1]
+    hinode[:,0] = hinode[:,0]*curve[0]+curve[1]
 
-    plt.figure()
-    plt.plot(lamda,hinode)
-    plt.plot(atlas[:,0],atlas[:,1])
-    plt.show()
-
-    #Plotting
+    #Plotting:
     if (plot=='yes'):
-        xmin1_fit = np.linspace(atlas[min1_pos-dlambda,0],atlas[min1_pos+dlambda,0],60)
-        ymin1_fit = min1_fit[0]*(xmin1_fit)**2 + min1_fit[1]*(xmin1_fit) + min1_fit[2]
-        xmin2_fit = np.linspace(atlas[min2_pos-dlambda,0],atlas[min2_pos+dlambda,0],60)
-        ymin2_fit = min2_fit[0]*(xmin2_fit)**2 + min2_fit[1]*(xmin2_fit) + min2_fit[2]
-        plt.vlines(x_min1_surr[min1fit_pos], ymin=2000, ymax = 10000,color='grey')
-        plt.vlines(x_min2_surr[min2fit_pos], ymin=2000, ymax = 10000,color='grey')
-        plt.scatter(atlas[min1_pos,0],atlas[min1_pos,1],c='r',s=10)
-        plt.scatter(atlas[min2_pos,0],atlas[min2_pos,1],c='r',s=10)
-        plt.plot(atlas[:,0],atlas[:,1],'k')
-        plt.plot(xmin1_fit, ymin1_fit)
-        plt.plot(xmin2_fit, ymin2_fit)
+        plt.figure()
+        plt.plot(atlas[:,0],atlas[:,1],label='Atlas data')
+        plt.plot(hinode[:,0],hinode[:,1],label='HINODE calibration')
+        plt.legend()
         plt.show()
+
+    return hinode[:,0]
 
 def Parabola(fit, x):
     return fit[0]*(x**2) + fit[1]*x + fit[2]
--- main.py
+++ main.py
@@ -2,9 +2,37 @@ from func import *
 FIGDIR = 'figures/'
 DATADIR = '/home/andy/Downloads/prac2solar/data/'
 
+# We are using the regions:
+#       - Calm Sun: 25:75,325:375
+#       - Dark spot: 175:200,225:250
 
-# data = Import_data(DATADIR+'Stokes_sunspot_HINODE.fits')
-# print_img(FIGDIR,data)
-# I_calm, I_dark = Import_regions(data)
-# Mean_Spectra_regions(DATADIR,I_calm, I_dark)
-Img_calibration(DATADIR, plot='yes')
+    #2# Calibration:
+#print_img(DATADIR+'Stokes_sunspot_HINODE.fits')
+lamda = Img_calibration(DATADIR, plot='no')
+
+    #3.1.1# Magnetic field:
+data = Import_data(DATADIR+'Stokes_sunspot_HINODE.fits')
+#I_V = np.vstack((lamda,np.add(np.mean(data[0,175:200,225:250,:],axis=(0,1)),
+#                 np.mean(data[3,175:200,225:250,:],axis=(0,1))).T)).T
+#print(I)
+#I_V_cont = np.mean(I_V[33:47,1])
+#min1 = Minimum_Finder(I_V,2,[10,30])
+#hmin = (I_V_cont - min1[1])/2 
+#
+#    #Plotting
+#plt.figure()
+#plt.plot(I_V[:,0],I_V[:,1])
+#plt.scatter(min1[0],hmin+min1[1],color='orange',s=30,marker='+',
+#            label='{0:.2f}'.format(hmin),zorder=2)
+#plt.scatter(min1[0],min1[1],color='orange',s=15,
+#            label='{0:.2f}'.format(min1[0])+', '+'{0:.2f}'.format(min1[1]),
+#            zorder=1)
+#plt.hlines(I_V_cont,xmin=np.min(I_V[:,0]),xmax=np.max(I_V[:,0]),color='r',
+#           ls='--',label='{0:.2f}'.format(I_V_cont))
+#plt.legend()
+#plt.show()
+
+    #3.1.3# Acimuth
+Q = data[1,:,:,:]
+U = data[2,:,:,:]
+phi = 0.5*np.arctan2(U,Q,)
